dc8f0638e1282c752b40dde20fe4f6629631047b
N3ST0R-JAG <nestorjulianarroyosgaray@gmail.com>
2026-07-15 21:36:29 -0300

    Enhance CORS settings in ApiComprasApplication
    Updated CORS configuration to allow additional HTTP methods.

--- APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
+++ APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
@@ -3,24 +3,26 @@ package com.articmeat.apicompras;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiComprasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiComprasApplication.class, args);
-	}
-	
-	@Bean
-	public WebMvConfigurer corsConfigurer(){
-		return new WebMvcConfigurer() {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiComprasApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
             }
-		};
-	}
+        };
+    }
 }
+
