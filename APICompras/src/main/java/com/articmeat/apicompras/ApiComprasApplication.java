e0a040c4c2fdc04b8d832948c05a436728677757
N3ST0R-JAG <nestorjulianarroyosgaray@gmail.com>
2026-07-15 21:32:16 -0300

    Fix import statement for web servlet configuration

--- APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
+++ APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
@@ -3,7 +3,7 @@ package com.articmeat.apicompras;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.serverlet.configuration.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class ApiComprasApplication {
