9cff90e8b3ab0d765db4cc07726d53f277ef1d98
N3ST0R-JAG <nestorjulianarroyosgaray@gmail.com>
2026-07-15 21:23:34 -0300

    Implement CORS configuration in ApiComprasApplication
    Added CORS configuration to allow all origins and methods.

--- APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
+++ APICompras/src/main/java/com/articmeat/apicompras/ApiComprasApplication.java
@@ -2,6 +2,8 @@ package com.articmeat.apicompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.serverlet.configuration.annotation.*:
 
 @SpringBootApplication
 public class ApiComprasApplication {
@@ -9,5 +11,16 @@ public class ApiComprasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiComprasApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+		};
+	}
 }
